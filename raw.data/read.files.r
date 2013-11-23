bf83ad8f8869431be3d6d2a433d4c670266492a5
Pedro J. Aphalo <pedro.aphalo@helsinki.fi>
2013-11-23 11:24:16 +0200

    Attemp to create .r files.

--- raw.data/read.files.r
+++ raw.data/read.files.r
@@ -10,10 +10,16 @@ rm(list = ls())
 setwd("raw.data")
 file.list <- system('ls *.csv', intern=TRUE)
 for (file.name in file.list) {
+  # data object
   df.name <- paste(sub(pattern=".csv", replacement="", x=file.name), "data", sep=".")
   df.name <- gsub(pattern="-", replacement="_", x=df.name)
   assign(df.name, read.csv(file.name, header=TRUE, comment.char="#"))
   save(list=df.name, file=paste("../data/", df.name, ".rda", sep=""))
+  # .r file with Roxygen2 doccumentation
+  r.file.name <- sub(".csv", ".r", file.name, fixed=TRUE)
+  system(paste('cp sensor.data.template.r', r.file.name))
+  system(paste("grep -U ^#", file.name, '>>', r.file.name))
+  system(paste('echo "NULL/n" >>', r.file.name))
 }
 setwd("./..")
 
