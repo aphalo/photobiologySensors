2f5eb7639bbf8a460ef82905660787e4d917ed13
Pedro J. Aphalo <pedro.aphalo@helsinki.fi>
2013-12-20 19:58:28 +0200

    Added one sensor.

---
+++ R/Solarmeter-SM60.r
@@ -0,0 +1,34 @@
+#' Spectral data for sensors
+#'
+#' Datasets containing the wavelengths (nm) at irregular intervals and
+#' tabulated values of spectral responsiveness for different sensors. 
+#' Spectral response values are in arbitrary units.
+#' 
+#' Digitized with 'enguage' from manufacturers brochures. This are approximate
+#' data, both because of the digitizing process, and because they are
+#' either typical values or for a particular sensor unit. Individual sensor units
+#' are spected to differ to some degree in spectral response.
+#' 
+#' The variables are as follows:
+#' \itemize{
+#'   \item w.length (nm)  
+#'   \item s.e.response (a.u.)
+#'   \item s.q.respone (a.u.) 
+#' }
+#' 
+#' @author Tähti Pohjanmies
+#' @author Pedro J. Aphalo \email{pedro.aphalo@@helsinki.fi} 
+#' @docType data
+#' @keywords datasets
+#'
+#' @name Solarmeter_SM60.data
+#' @note
+#' SOLARMETER MODEL 6.0 UV METER
+#' Silicon Carbide (SIC) Photodiode packaged in hermetically sealed UV glass window cap
+#' Interference filter coating (Metal Oxide) blocks most UVA
+#' Manufacturer: Solartech, Inc., 26101 Harbour Pointe Dr N., Harrison Twp, MI 48045
+#' \url{https://www.solarmeter.com/images/SM60graph.gif} on 20 December 2013
+#' 
+#' @references digitized from SM60graph.gif
+#'
+"NULL" 
